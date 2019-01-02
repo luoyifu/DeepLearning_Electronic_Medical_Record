ea9e2f55ac2295d8b99e002a36791ce764564106
Luo Yifu <luoyifu2013@163.com>
2019-01-02 21:58:26 +0800

    主诉，检查，诊断信息处理
    整理规范化了EMR_exam_zhusu_read

--- EMR_exam_zhusu_read.py
+++ EMR_exam_zhusu_read.py
@@ -22,32 +22,41 @@ import Text_Treatment
 import Pre_Treatment
 import EMR_read_FeatureAbstract as erf
 
+# ---------------------------文件准备---------------------------#
 name1='辅助检查'
 name2='主 诉'
 name3='最后诊断'
+# 病历数据库所在文件路径
 file_path = (
     u'C:/workspace/research/EMR_Database/患者病历文本语料仓库_test_database')
+# 处理后的数据存储文件路径
+filepath_after_treatment_text_database = (
+    u'C:/workspace/research/EMR_Database/temp/患者病历处理后的文本语料仓库_database/')
 
 # 获取文件名称列表
 filelist_exam = Text_Treatment.get_full_filename(file_path,name1)
 filelist_zhusu = Text_Treatment.get_full_filename(file_path,name2)
 filelist_zhenduan = Text_Treatment.get_full_filename(file_path,name3)
-# word_list_test = []
-# word_list_test.append(read_exam_file_single(filelist_exam[1]))
 
+# ----------------------------数据读取-----------------------------#
+# 用list存储读取到的病历数据，每个特征短语存储为list的一个元素
 word_list_exam = []
 word_list_zhusu = []
+word_list_zhenduan = []
 
+# 读取’辅助检查‘信息
 for x in filelist_exam:
     word_list_exam.append(erf.read_exam_file_single(x))
-
+# 读取’主诉‘信息
 for x in filelist_zhusu:
     word_list_zhusu.append(erf.read_exam_file_single(x))
+# 读取’最后诊断‘信息
+for x in filelist_zhenduan:
+    word_list_zhenduan.append(erf.read_zhenduan_file(x))
 
 # 将2个词汇列表合并为1个
 # 每个病历的主诉和检验结果都存放在一个list中，即word_list_patient
-filepath_after_treatment_text_database = (
-    u'C:/workspace/research/EMR_Database/temp/患者病历处理后的文本语料仓库_database/')
+
 
 word_list_patient = word_list_exam
 for i in range(len(filelist_exam)):
@@ -57,53 +66,55 @@ for i in range(len(filelist_exam)):
     filename_temp = ''.join(temp)
     Text_Treatment.write_word_list_to_file(word_list_patient[i],filename_temp)
 
+# -----------------------------非重复特征提取------------------------------#
 # 提取患者病历中所有非重复特征
 # 对word_list_patient进行非重复特征提取，提取特征存入exam_feature_list
 exam_feature_list = []
 for x in word_list_patient:
     exam_feature_list = erf.find_unique_feature(exam_feature_list, x)
 
+zhenduan_feature_list = []
+for x in word_list_zhenduan:
+    zhenduan_feature_list = erf.find_unique_feature(zhenduan_feature_list, x)
+
+# -------------------------------tfidf计算--------------------------------#
 # 计算exam_feature_list中每个特征的idf值
 feature_idf_list = erf.feature_idf(exam_feature_list, word_list_patient) 
-
+# 特征进行词袋模型转化，转变成向量
 feature_EMR = []
+# 特征向量计算tfidf值
 feature_EMR_tfidf = []
 for x in word_list_patient:
     feature_EMR.append(erf.feature_EMR_exam_zhusu(exam_feature_list, x))
     feature_EMR_tfidf.append(erf.feature_tfidf(erf.feature_EMR_exam_zhusu(exam_feature_list, x), feature_idf_list))
 
-feature_EMR_np_array = np.array(feature_EMR)
-feature_EMR_tfidf_np_array = np.array(feature_EMR_tfidf)
-
-np.save('feature_EMR_np_array.npy',feature_EMR_np_array)
-np.save('feature_EMR_tfidf_np_array.npy',feature_EMR_tfidf_np_array)
-
-# 读取最后诊断信息
-word_list_zhenduan = []
-for x in filelist_zhenduan:
-    word_list_zhenduan.append(erf.read_zhenduan_file(x))
-
-zhenduan_feature_list = []
-for x in word_list_zhenduan:
-    zhenduan_feature_list = erf.find_unique_feature(zhenduan_feature_list, x)
-
-zhenduan_idf_list = erf.feature_idf(zhenduan_feature_list, word_list_zhenduan) 
-
+# 计算zhenduan_feature_list中每个特征的idf值
+zhenduan_idf_list = erf.feature_idf(zhenduan_feature_list, word_list_zhenduan)
+# 特征进行词袋模型转化，转变成向量 
 zhenduan_EMR = []
+# 特征向量计算tfidf值
 zhenduan_EMR_tfidf = []
 for x in word_list_zhenduan:
     zhenduan_EMR.append(erf.feature_EMR_exam_zhusu(zhenduan_feature_list, x))
     zhenduan_EMR_tfidf.append(erf.feature_tfidf(erf.feature_EMR_exam_zhusu(zhenduan_feature_list, x), zhenduan_idf_list))
 
+# -------------------------------特征向量转换成numpy array型------------------------------#
+feature_EMR_np_array = np.array(feature_EMR)
+feature_EMR_tfidf_np_array = np.array(feature_EMR_tfidf)
 zhenduan_EMR_np_array = np.array(zhenduan_EMR)
 zhenduan_EMR_tfidf_np_array = np.array(zhenduan_EMR_tfidf)
 
+np.save('feature_EMR_np_array.npy',feature_EMR_np_array)
+np.save('feature_EMR_tfidf_np_array.npy',feature_EMR_tfidf_np_array)
 np.save('zhenduan_EMR_np_array.npy',zhenduan_EMR_np_array)
 np.save('zhenduan_EMR_tfidf_np_array.npy',zhenduan_EMR_tfidf_np_array)
 
 print(physical_exam_feature_list)
 
+
 '''
+【结果分析】：
+
 提取主诉和检验数据
 病历总数 N
 得到exam_feature_list是8922行，即共有8922个特征。计为 M
@@ -116,14 +127,10 @@ print(physical_exam_feature_list)
 
 提取诊断数据特征
 zhenduan_feature_list共6780行，即6780个诊断短语。但是，这些诊断结果有很多是相似的，只是表述方式不同。
-
 与提取主诉和检验数据特征类似的，提取诊断特征。zhenduan_EMR
 
 因此，一个问题是如何度量诊断结果的相似性，或者用什么方式将不同的表述归化为统一表述。
 
-
 我们面临的一个主要问题是，特征向量，或者所有病例特征所形成的特征矩阵太稀疏。如何去稀疏化？
-
-诊断向量
 
 '''
