c798a2a0dea76d5c5cbdfe7ce022a0934075f22a
Luo Yifu <luoyifu2013@163.com>
2019-01-03 20:55:08 +0800

    working on it

--- research1901.py
+++ research1901.py
@@ -4,8 +4,14 @@
 
 主要针对主诉，辅助检查和最终诊断做分析
 
+method:
+1. 
+计算病历之间的cos相似性，与诊断结果之间的cos相似性，
+比较两组相似性，差值按照每0.1一个区间进行统计
+
 '''
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 # ------------------------数据准备---------------------------------#
@@ -18,7 +24,7 @@ zhenduan_EMR_tfidf_np_array = np.load('zhenduan_EMR_tfidf_np_array.npy')
 
 
 # -------------------------计算所需函数------------------------------#
-# 定义计算余弦相似性函数
+# 计算两个向量的余弦相似性函数
 def cos_sim(array1,array2):
     num = float(array1 .dot(array2)) 
     denom = np.linalg.norm(array1) * np.linalg.norm(array2)
@@ -26,5 +32,30 @@ def cos_sim(array1,array2):
     sim = 0.5 + 0.5 * cos #归一化
     return sim
 
-c1 = cos_sim(zhenduan_EMR_np_array[0],zhenduan_EMR_np_array[1])
-c2 = cos_sim(zhenduan_EMR_np_array[0],zhenduan_EMR_np_array[2])
+# 绘制直方图函数。
+# 需要输入numpy array数据，绘制共20个区间的直方图
+def show_histogram(data):
+    n, bins, patches = plt.hist(data, 20,normed=0, edgecolor="black")
+    plt.xlabel("diff range")
+    # 显示纵轴标签
+    plt.ylabel("frequency")
+    plt.title("diff between feature sim and zhenduan sim")
+    plt.show()
+
+# -----------------------------分析-----------------------------------#
+
+# method 1.实现
+diff = []
+for i in range(len(zhenduan_EMR_np_array)):
+    for j in range(len(zhenduan_EMR_np_array)):
+        if i !=j:
+            cos_feature = cos_sim(feature_EMR_np_array[i],feature_EMR_np_array[j])
+            cos_zhenduan = cos_sim(zhenduan_EMR_np_array[i],zhenduan_EMR_np_array[j])
+            diff.append(abs(cos_feature - cos_zhenduan))
+            if abs(cos_feature - cos_zhenduan) == 0:
+                print(i,j)
+
+# diff数据转换成numpy array
+diff_array = np.array(diff)
+
+show_histogram(diff_array)
